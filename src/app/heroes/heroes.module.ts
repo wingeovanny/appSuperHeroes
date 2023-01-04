f2b70e309d8774df9686121688c1499affd39542
Geo Bucay <ebucayle@pichincha.com>
2023-01-03 23:21:38 -0500

    lista de heroes arquitectura

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -1,6 +1,9 @@
+import { MaterialModule } from './../material/material.module';
 import { HeroesRoutingModule } from './heroes-routing.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { AgregarComponent } from './pages/agregar/agregar.component';
 import { BuscarComponent } from './pages/buscar/buscar.component';
 import { HeroeComponent } from './pages/heroe/heroe.component';
@@ -15,6 +18,11 @@ import { ListarComponent } from './pages/listar/listar.component';
     HomeComponent,
     ListarComponent,
   ],
-  imports: [CommonModule, HeroesRoutingModule],
+  imports: [
+    CommonModule,
+    FlexLayoutModule,
+    HeroesRoutingModule,
+    MaterialModule,
+  ],
 })
 export class HeroesModule {}
